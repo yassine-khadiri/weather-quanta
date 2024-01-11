17b5a5dfbb0b46cb3339ae686d07984e036cab6e
yassine-khadiri <yassinekhadiri77@gmail.com>
2024-01-11 19:51:19 +0100

    Add Some Styling And Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { CiLocationOn } from "react-icons/ci";
-import { FaCloud } from "react-icons/fa6";
+// import { FaCloud } from "react-icons/fa6";
 import { GiWindsock } from "react-icons/gi";
 import { MdOutlineWaterDrop } from "react-icons/md";
 import { RiCelsiusLine } from "react-icons/ri";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { monthsOfYear, weatherImages } from "./utils/data";
+// import moroccoCities from "./utils/morocco-cities.json";
 
 function App() {
   const currentTimestamp: number = Date.now();
@@ -13,6 +15,9 @@ function App() {
   const year: number = currentDate.getFullYear();
   const month: number = currentDate.getMonth();
   const day: number = currentDate.getDate();
+  const [locationInfos, setLocationInfos] = useState<any>("");
+
+  const [weatherData, setWeatherData] = useState<any>();
 
   // const daysOfWeek : string[] = [
   //   "Sunday",
@@ -24,51 +29,128 @@ function App() {
   //   "Saturday",
   // ];
 
-  const monthsOfYear = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  // const [moroccoCities, setMoroccoCities] = useState<{ city: string }[]>([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=cb2eca08845cfae37d80b7db6744e510`,
+
+  // const getMoroccoCites = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.value.trim().length)
+  //     setMoroccoCities(() => moroccoCities.map((elem) => elem.city));
+  //   else setMoroccoCities([]);
+  // };
+
+  const findAppropriateIcon = (): string => {
+    for (let i = 0; i < weatherImages.length; i++) {
+      if (weatherImages[i].main === weatherData?.weather[0].main) {
+        let j = 0;
+        for (j; j < weatherImages[i].description.length; j++) {
+          if (
+            weatherImages[i].description[j].value ===
+            weatherData?.weather[0].description
+          ) {
+            console.log(weatherImages[i].description[j].icons[0]);
+            return weatherImages[i].description[j].icons[0];
+          }
+        }
+      }
+    }
+    return "";
+  };
+
+  const getWeatherData = async () => {
+    try {
+      const locationData = await axios.get(
+        `https://ipinfo.io/json?token=${import.meta.env.VITE_IP_INFO_TOKEN}`,
       );
 
-      console.log(data.data);
-    };
-    getData();
+      setLocationInfos(locationData.data);
+
+      const latLongData = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${
+          locationData.data.city
+        }&appid=${import.meta.env.VITE_WEATHER_API_KEY}`,
+      );
+
+      const data = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${
+          latLongData.data[0].lat
+        }&lon=${latLongData.data[0].lon}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units=metric`,
+      );
+      setWeatherData(data.data);
+    } catch {
+      console.log("Connot Get Location Infos!");
+    }
+  };
+
+  useEffect(() => {
+    getWeatherData();
   }, []);
+
+  // city: "Khouribga";
+  // country: "MA";
+  // ip: "197.230.30.146";
+  // loc: "32.8811,-6.9063";
+  // org: "AS36925 MEDITELECOM";
+  // postal: "25100";
+  // region: "Béni Mellal-Khénifra";
+  // timezone: "Africa/Casablanca";
 
   return (
     <>
+      {/* {console.log(weatherData.weather)} */}
       <div className="relative flex h-screen w-full items-center justify-center bg-[url('./assets/cover.jpeg')] bg-cover bg-no-repeat">
         <div className="container flex h-full flex-col items-end justify-center gap-10">
-          <input
-            type="text"
-            placeholder="Search For A Country"
-            className="glassmorphism w-[300px] px-5 py-2"
-          />
+          <div className="relative w-[300px] rounded-xl">
+            <input
+              type="text"
+              placeholder="Search For A City"
+              className="glassmorphism w-[300px] px-5 py-2"
+              // onChange={(e) => getMoroccoCites(e)}
+            />
+            {/*<div className="glassmorphism backdr absolute z-10 mt-2 w-[300px] rounded-xl px-5 py-3 backdrop-blur-[13px]">
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+              <div className="w-full cursor-pointer p-3 duration-300 ease-in-out hover:bg-[rgba(255,255,255,.3)]">
+                hello
+              </div>
+            </div>*/}
+          </div>
           <div className="glassmorphism relative flex h-[60%] w-full items-center justify-around px-10 py-5">
             <span className="absolute right-10 top-10 mr-3 text-xl">{`${day} ${monthsOfYear[month]}, ${year}`}</span>
             <div className="flex flex-col gap-10 p-4 text-5xl">
               <div className="flex items-end gap-3">
                 <CiLocationOn className="text-6xl" />
-                <span className="text-5xl">Azemmour</span>
+                <span className="text-5xl">{locationInfos?.city || "-"}</span>
               </div>
               <div className="flex flex-col">
-                <FaCloud className="text-[150px]" />
-                <span className="-mt-[15px] ml-1 inline-block text-[40px]">
+                {/* <FaCloud className="text-[150px]" /> */}
+                <div className="h-[130px] w-[150px] text-[150px]">
+                  {/* <img
+                    src={`http://openweathermap.org/img/w/${weatherData?.weather[0].icon}.png`}
+                    alt="weather icon"
+                    className="w-full"
+                  /> */}
+                  <img
+                    src={findAppropriateIcon()}
+                    alt="weather icon"
+                    className="w-full"
+                  />
+                </div>
+                <span className="ml-1 mt-[35px] inline-block text-[40px]">
                   Cloudy
                 </span>
               </div>
@@ -77,14 +159,20 @@ function App() {
             <div className="flex h-fit gap-5 border-l-2 border-white">
               <div className="-mb-24 flex items-center gap-3 px-5 text-2xl">
                 <MdOutlineWaterDrop className="text-4xl" />
-                <span>10%</span>
+                <span>{`${
+                  Math.round(weatherData?.main.humidity) || "-"
+                } %`}</span>
               </div>
               <div className="-mb-24 flex items-center gap-3 px-5 text-2xl">
                 <GiWindsock className="text-4xl" />
-                <span>10m/s</span>
+                <span>{`${
+                  Math.round(weatherData?.wind.speed) || "-"
+                } m/s`}</span>
               </div>
               <div className="relative px-5">
-                <span className="mr-14 text-[200px]">10</span>
+                <span className="mr-14 text-[200px]">
+                  {Math.round(weatherData?.main.temp) || "-"}
+                </span>
                 <RiCelsiusLine className="absolute bottom-[75px] right-0 text-2xl text-[65px]" />
               </div>
             </div>
---
+++ src/utils/data.ts
@@ -0,0 +1,242 @@
+export const monthsOfYear: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const weatherImages = [
+  {
+    main: "Clear",
+    description: [
+      {
+        value: "clear sky",
+        icons: [
+          "/assets/weather-icons/01d.png",
+          "/assets/weather-icons/01n.png",
+        ],
+      },
+    ],
+  },
+  {
+    main: "Clouds",
+    description: [
+      {
+        value: "few clouds",
+        icons: [
+          "/assets/weather-icons/02d.png",
+          "/assets/weather-icons/02n.png",
+        ],
+      },
+      {
+        value: "scattered clouds",
+        icons: [
+          "/assets/weather-icons/03d.png",
+          "/assets/weather-icons/03d.png",
+        ],
+      },
+      {
+        value: "broken clouds",
+        icons: [
+          "/assets/weather-icons/04d.png",
+          "/assets/weather-icons/04d.png",
+        ],
+      },
+      {
+        value: "overcast clouds",
+        icons: [
+          "/assets/weather-icons/04d.png",
+          "/assets/weather-icons/04d.png",
+        ],
+      },
+    ],
+  },
+  {
+    main: "Drizzle",
+    description: [
+      {
+        value: "light intensity drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "heavy intensity drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "light intensity drizzle rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "drizzle rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "heavy intensity drizzle rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "shower rain and drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "heavy shower rain and drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "shower drizzle",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+    ],
+  },
+  {
+    main: "Rain",
+    description: [
+      {
+        value: "light rain",
+        icons: ["/assets/weather-icons/10d.png"],
+      },
+      {
+        value: "moderate rain",
+        icons: ["/assets/weather-icons/10d.png"],
+      },
+      {
+        value: "heavy intensity rain",
+        icons: ["/assets/weather-icons/10d.png"],
+      },
+      {
+        value: "very heavy rain",
+        icons: ["/assets/weather-icons/10d.png"],
+      },
+      {
+        value: "extreme rain",
+        icons: ["/assets/weather-icons/10d.png"],
+      },
+      {
+        value: "freezing rain",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "light intensity shower rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "shower rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "heavy intensity shower rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+      {
+        value: "ragged shower rain",
+        icons: ["/assets/weather-icons/09d.png"],
+      },
+    ],
+  },
+  {
+    main: "Thunderstorm",
+    description: [
+      {
+        value: "thunderstorm with light rain",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm with rain",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm with heavy rain",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "light thunderstorm",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "heavy thunderstormn",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "ragged thunderstorm",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm with light drizzle",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm with drizzle",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+      {
+        value: "thunderstorm with heavy drizzle",
+        icons: ["/assets/weather-icons/11d.png"],
+      },
+    ],
+  },
+  {
+    main: "Snow",
+    description: [
+      {
+        value: "light snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "heavy snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "sleet",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "light shower sleet",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "shower sleet",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "light rain and snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "rain and snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "light shower snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "shower snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+      {
+        value: "heavy shower snow",
+        icons: ["/assets/weather-icons/13d.png"],
+      },
+    ],
+  },
+];
